89ce04ff159b0bf653285c720aee994a519992bf
RoiArthurB <contact@arthurbrugiere.fr>
2020-05-04 17:20:30 +0700

    [JAVA] Enhance verbose Always great to be nice :3

--- scripts/generateJavaConfFile.py
+++ scripts/generateJavaConfFile.py
@@ -82,3 +82,7 @@ if __name__ == '__main__':
 	#
 	print("=== Generate SLURM command")
 	print("srun --wckey=ird:gama -n " + str(args.core) + " --partition=cn -J COMOKIT --exclusive -N " + str(args.node) + " ----ntasks-per-node=" + str(args.core) + " --time 420 -o log.txt --comment COMOKIT --multi-prog gama-headless.conf &")
+
+	# 4 _ Done
+	#
+	print("\n=== Done ;)")
