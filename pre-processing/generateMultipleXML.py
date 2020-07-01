abe44990fd2c1c460bddd28dfd8aebfc8836e8ae
RoiArthurB <contact@arthurbrugiere.fr>
2020-07-01 16:35:43 +0700

    [XML] Output path is relative to GAML file

--- pre-processing/generateMultipleXML.py
+++ pre-processing/generateMultipleXML.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(usage='$ python3 %(prog)s [options] -f INT -xml <experiment name> /path/to/file.gaml /path/to/file.xml')
 	parser.add_argument('-r', '--replication', metavar='INT', help="Number of replication for each paramater space (default: 1)", default=1, type=int)
 	parser.add_argument('-s', '--split', metavar='INT', help="Split XML file every S replications (default: 1)", default=-1, type=int)
-	parser.add_argument('-o', '--output', metavar='STR', help="Path to folder where save output CSV (default: \"../../batch_output\")", default="../../batch_output", type=str)
+	parser.add_argument('-o', '--output', metavar='STR', help="Relative path from GAML file to folder where save output CSV (default: \"../../batch_output\" => /path/to/COMOKIT/batch_output)", default="../../batch_output", type=str)
 	parser.add_argument('-u', '--until', metavar='STR', help="Stop condition for the simulations (default: \"world.sim_stop()\"", default="world.sim_stop()", type=str)
 	parser.add_argument('-S', '--seed', metavar='INT', help="Starting value for seeding simulation (default: 0)", default=0, type=int)	
 	parser.add_argument('-f', '--final', metavar='INT', help="Final step for simulations", default=-1, type=int,  required=True)
@@ -138,7 +138,7 @@ if __name__ == '__main__':
 	# Turn them all in absolute path
 	gamlFilePath = os.path.abspath(gamlFilePath)
 	xmlFilePath = os.path.abspath(xmlFilePath)
-	args.output = os.path.abspath(args.output)
+	args.output = os.path.abspath(os.path.split(gamlFilePath)[0] + "/" + args.output)
 
 	# Prevent wrong path
 	if args.output[-1] == "/":
