38d825d0cbaab93435647089ebf91e97d1d0346c
RoiArthurB <contact@arthurbrugiere.fr>
2020-10-08 14:11:21 +0700

    [C2png] Fully working and optimized script Multi-cpu processing Multi-graph generation

--- post-processing/comokit2png.py
+++ post-processing/comokit2png.py
@@ -41,9 +41,7 @@ parser.add_argument('-q', '--quiet', action='store_true', help='Disable verbose 
 parser.add_argument('-c', '--cores', metavar='', help="Number of core to use (default: max number of cores)", default=multiprocessing.cpu_count(), type=int)
 parser.add_argument('-s', '--stepTo', metavar='', help="Change step displayed in the graph (default: 24 -> day)", default=24, type=int)
 
-
 args = parser.parse_args()
-
 
 # 1 _ Gathering COMOKIT datasets
 # 
@@ -62,79 +60,79 @@ for csv_file in onlyfiles:
 # 2.1 _ Prepare variables/functions for processing
 # 
 
-# Aggregation + Data processing
-outputSick = []
-outputAsymp = []
-outputSymp = []
-prevSum = 0
-nbrSim = args.replication
-index_graph = 0
+output_name  = ["Asymptomatic", "Symptomatic", "Recovered", "Death"]
+output_color = ["blue", "red", "green", "black"]
 
-def processPerHour(index, graph):
+# Aggregation + Data processing
+def processPerHour(index, graph, outputs):
+    prevSum = 0
     # Set/Clear for new line
     CSVs_sick = pd.DataFrame()
-    CSVs_asymptomatic = pd.DataFrame()
-    CSVs_symptomatic = pd.DataFrame()
+    output_CSVs = [pd.DataFrame() for i in range(len(output_name))]
 
     # Per file
     for csv in CSVs:
-        for hour in range(args.stepTo):            
-            if len(csv[5]) > (index + hour):
-                # ['total_incidence', 'need_hosp', 'need_icu', 'susceptible', 'latent', 'asymptomatic', 'presymptomatic', 'symptomatic', 'recovered', 'dead']
-                # [     0                   1           2           3             4         5               6                   7             8         9   ]
+        for hour in range(args.stepTo):
+            # Gather data per col/row
+            if len(csv[5]) > (index + hour): # Check if row exist
                 # Gather line data in every file
-                CSVs_asymptomatic = CSVs_asymptomatic.append([int(csv[5][index + hour])])
-                CSVs_symptomatic = CSVs_symptomatic.append([int(csv[6][index + hour])])
-                CSVs_symptomatic = CSVs_symptomatic.append([int(csv[7][index + hour])])
+                #CSVs_sick = CSVs_sick.append([int(csv[0][index])])                         # total_incidence
+                # need_hosp
+                # need_icu
+                # susceptible
+                output_CSVs[0] = output_CSVs[0].append([int(csv[5][index + hour])])   # asymptomatic
+                output_CSVs[1] = output_CSVs[1].append([int(csv[6][index + hour])])   # presymptomatic
+                output_CSVs[1] = output_CSVs[1].append([int(csv[7][index + hour])])   # symptomatic
+                output_CSVs[2] = output_CSVs[2].append([int(csv[8][index + hour])])   # recovered
+                output_CSVs[3] = output_CSVs[3].append([int(csv[9][index + hour])])   # dead
     
-    # Process asymptomatic
-    # [min, max, mean]
-    asymptoVariance = float(CSVs_asymptomatic.std())
-    asymptoMean = float(CSVs_asymptomatic.mean())
-    outputAsymp.insert(graph, [
-        max(0.0, (asymptoMean - asymptoVariance)),
-        (asymptoMean + asymptoVariance),
-        asymptoMean
-        ])
-
-    # Process symptomatic
-    # [min, max, mean]
-    symptoVariance = float(CSVs_symptomatic.std())
-    symptoMean = float(CSVs_symptomatic.mean())
-    outputSymp.insert(graph, [
-        max(0.0, (symptoMean - symptoVariance)),
-        (symptoMean + symptoVariance),
-        symptoMean
-        ])
+    # Process data
+    for i in range(len(output_name)):
+        variance = float(output_CSVs[i].std())
+        mean = float(output_CSVs[i].mean())
+        
+        # [min, max, mean]
+        outputs[i][graph] = [
+            max(0.0, (mean - variance)),
+            (mean + variance),
+            mean
+            ]
 # !def processPerHour
 
-def splitPerProcess(mini, maxi, index_graph):
+def splitPerProcess(mini, maxi, index_graph, outputs):
     for row in range(mini, maxi, args.stepTo):
         if row > len(CSVs[0]):
             continue
 
-        processPerHour(row, index_graph)
+        # Process a row
+        processPerHour(row, index_graph, outputs)
 
         index_graph += 1
 
     # Verbose
     if not args.quiet:
-        print("End thread processing lines ["+str(mini)+","+str(maxi)+"[")
+        print("End thread processing lines ["+str(mini)+","+str(maxi)+"] with end index " + str(index_graph))
 # !def splitPerProcess
 
 
 # 2.2 _ Launch processing
 # 
 
+lenCSVs = len(CSVs[0])
 threads = []
-print("Start thread processing...")
+manager = multiprocessing.Manager()
+output = [manager.list(range( int(lenCSVs/args.stepTo) )) for i in range(len(output_name))]
+
+if not args.quiet:
+    print("Start thread processing...")
 # Create a thread per core
 for split in range(args.cores):
 
-    step = int(len(CSVs[0]) / args.cores)
+    step = int(lenCSVs / args.cores)
     mini = int(step * split)
 
-    x = multiprocessing.Process(target=splitPerProcess, args=(mini, (mini + step -1), int(mini / args.stepTo) ) )
+    # Start a thread on a core
+    x = multiprocessing.Process(target=splitPerProcess, args=(mini, (mini + step -1), int(mini / args.stepTo), output ) )
     threads.append(x)
     x.start()
 
@@ -142,31 +140,38 @@ for split in range(args.cores):
 for index, thread in enumerate(threads):
     thread.join()
 
+if not args.quiet:
+    print("Quick view of some processed data :")
+    print(output[0][620:630])
+    print("Processed " + str(len(output)) + " days")
 
 # 3 _ Generating image
 # 
 
-print("Creating plot...")
-# Turn result in user-friendly DataFrame
+if not args.quiet:
+    print("Creating plot...")
+
 col_name = ["Min", "Max", "Mean"]
 
-time = pd.date_range('1/1/2000', periods=len(CSVs), freq='1h')
-df_tmp = pd.DataFrame(outputAsymp, columns=col_name)
-df_tmp1 = pd.DataFrame(outputSymp, columns=col_name)
+# Turn result in user-friendly DataFrame
+output_df = []
+for i in range(len(output)):
+    output_df.append( pd.DataFrame(list(output[i]), columns=col_name) )
 
 # Initialise the figure and axes.
-fig, ax = plt.subplots(1, 2, sharey=True)
+fig, ax = plt.subplots(1, len(output_df), figsize=(10,3), sharey=True)
 
 fig.suptitle( args.title )
 
 # Set curves
-ax[0].fill_between(df_tmp1.index, df_tmp["Min"], df_tmp["Max"], color='b', alpha=0.2, label = "Min/Max")
-ax[0].plot(df_tmp1.index, df_tmp["Mean"], label = "Mean")
-ax[0].legend(loc="upper left", title="Asymp", frameon=True)
+for i in range(len(output_df)):
+    ax[i].fill_between(output_df[i].index, output_df[i]["Min"], output_df[i]["Max"], color=output_color[i], alpha=0.2, label = "Min/Max")
+    ax[i].plot(output_df[i].index, output_df[i]["Mean"], color=output_color[i], label = "Mean")
+    ax[i].legend(loc="upper left", title=output_name[i], frameon=True)
 
-ax[1].fill_between(df_tmp1.index, df_tmp1["Min"], df_tmp1["Max"], color='r', alpha=0.2, label = "Min/Max")
-ax[1].plot(df_tmp1.index, df_tmp1["Mean"], label = "Mean", color='r')
-ax[1].legend(loc="upper left", title="Symp", frameon=True)
+# Set axes legends
+plt.setp(ax[-1], xlabel='Days')
+plt.setp(ax[:], ylabel='Number of person')
 
 # Save output graph
 imgName = args.outputImg + str(len([f for f in listdir("./") if isfile(join("./", f)) and ("out" in f)])) + '.png'
