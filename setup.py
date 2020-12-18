345b011f0a906ca57479563cfa1ff67752f4f2bf
RoiArthurB <contact@arthurbrugiere.fr>
2020-12-18 11:19:15 +0700

    [INIT] Push package skeleton

---
+++ setup.py
@@ -0,0 +1,28 @@
+# File created following this official tutorial
+# https://packaging.python.org/tutorials/packaging-projects/
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="comokit4py",  # Replace with your own username
+    version="0.0.1",    # Format : https://www.python.org/dev/peps/pep-0440/
+    author="COMOKIT",
+    author_email="contact@comokit.org",
+    description="Python3 library to simplify the exploration of COMOKIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/COMOKIT/comokit4py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Other",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires='>=3.6',
+)
