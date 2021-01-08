34ce3d423c750016f7072ce779e0f98cabd6e965
RoiArthurB <contact@arthurbrugiere.fr>
2021-01-08 10:46:21 +0700

    [LIB] Fill GamaExploration class

--- comokit4py/__init__.py
+++ comokit4py/__init__.py
@@ -76,4 +76,62 @@ class exploration:
 		comokit2png.savePngGraphs()
 	#!
 
-	#def 
+	#
+	#	BASE
+	#
+	def __init__(self, experimentName : str, gamlFile : str, xmlOutputName : str, replication : int, experimentPerXML : int, final : int, until : str = "", seed : int = 0):
+		self.experimentName = experimentName
+		self.gamlFile = gamlFile
+		self.xmlOutputName = xmlOutputName
+		self.replication = replication
+		self.experimentPerXML = experimentPerXML
+		self.final = final
+		self.until = until
+		self.seed = seed
+	#! __init__
+
+	#
+	#	GET/SET
+	#
+	def getExperimentName() -> str:
+		return self.experimentName
+	def setExperimentName(experimentName : str) -> None:
+		self.experimentName = experimentName
+
+	def getGamlFile() -> str:
+		return self.gamlFile
+	def setGamlFile(gamlFile : str) -> None:
+		self.gamlFile = gamlFile
+
+	def getXmlOutputName() -> str:
+		return self.xmlOutputName
+	def setXmlOutputName(xmlOutputName : str) -> None:
+		self.xmlOutputName = xmlOutputName
+
+	def getReplication() -> int:
+		return self.replication
+	def setReplication() -> None:
+		self.replication = replication
+
+	def getExperimentPerXML() -> int:
+		return experimentPerXML
+	def setExperimentPerXML(experimentPerXML : int) -> None:
+		self.experimentPerXML = experimentPerXML
+
+	def getFinal() -> int:
+		return self.final
+	def setFinal(final: int) -> None:
+		self.final = final
+
+	def getUntil() -> str:
+		return self.until
+	def setUntil(until : str) -> None:
+		self.until = until
+
+	def getSeed() -> int:
+		return self.seed
+	def setSeed(seed : int) -> None:
+		self.seed = seed
+	#! GET/SET
+
+#! GamaExploration
