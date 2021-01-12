6f683624a0d50813a15fd61e35c77a4d1e94281c
RoiArthurB <contact@arthurbrugiere.fr>
2021-01-12 16:27:32 +0700

    [TEST] Add test-launchingExplo.py

---
+++ test/test-launchingExplo.py
@@ -0,0 +1,28 @@
+import comokit4py
+import os
+
+#
+#	Setup GAMA
+#	cf. test-gama.py
+#
+gamaPath = "../../../../.local/share/GAMA_1.8.1_Linux"
+gamaPathHeadless = os.path.join(gamaPath,"headless/gama-headless.sh")
+
+gama = comokit4py.Gama(gamaPathHeadless)
+
+#
+#	Setup GAMA Exploration
+#	cf. test-gamaExp.py
+#
+explo = comokit4py.GamaExploration(experimentName = "Headless", gamlFile = "/home/roiarthurb/Documents/COMOKIT/Model/COMOKIT/Experiments/Physical Interventions/Significance of Wearing Masks.gaml", replication = 2, final = 2, experimentPerXML  = 8)
+explo.calculatesExperimentSpace()
+
+#
+#	Setup GAMA Exploration Workspace
+#
+ws = comokit4py.Workspace(gama, explo, "./out", True)
+
+print("Verify all :", ws.verifyAll())
+
+ws.generateNeededForExploration()
+ws.runGamaHeadless()
