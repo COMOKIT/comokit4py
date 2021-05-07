51f0f8f06ac3432602f129e8c2de1950e61f8252
ndgnuh <ndgnuh@protonmail.com>
2021-05-07 16:57:49 +0700

    Gather all population

--- base/post-processing/comokitreport.py
+++ base/post-processing/comokitreport.py
@@ -1,7 +1,7 @@
 import os
 import os.path as path
 import pandas as pd
-import numpy as pd
+import numpy as np
 from comomkit4py.comokit2png import gatheringCSV
 
 # list of columns ordered by appearing order in csv
@@ -13,6 +13,7 @@ __columns = [
 		"latent",
 		"asymptomatic",
 		"presymtomatic",
+		"symtomatic",
 		"recovered",
 		"dead"
 		]
@@ -20,37 +21,31 @@ __columns = [
 # An invert indices for easier lookup
 __invColumns = dict(map(lambda pair: (pair[1], pair[0]), enumerate(__columns)))
 
-def mapreduce(fmap, freduce, itr, initial = None):
+def mapreduce(fmap, freduce, itr):
 	"""
-	mapreduce(fmap, freduce, itr[, initial])
+	mapreduce(fmap, freduce, itr)
 	---
-	Perform `map(fmap, itr)`, then perform `functools.reduce(freduce, result)` and return the final result. Initial is passed to reduce
+	Perform `map(fmap, itr)`, then perform `functools.reduce(freduce, result)` and return the final result.
 
 	mapreduce(lambda x: x + 1, lambda x, y: x * y, [0,1,2]) == 6
 	"""
 	mapped = map(fmap, itr)
-	if initial is None:
-		return fp.reduce(freduce, mapped)
-	else:
-		return fp.reduce(freduce, mapped, initial = initial)
+	return fp.reduce(freduce, mapped)
 
-def __columnGetter(field):
-	"""
-	__columnGetter(field)
-	---
-	Return a function that get the corresponding column in the output data. `field` must be one of `__columns`.
-	"""
-	assert(field in __columns)
-	index = __invColumns[field]
-	def aux(data):
-		return data[:, index]
-	
-getTotal = __columnGetter("total")
-getHospitalisation = __columnGetter("hospitalisation")
-getICU = __columnGetter("ICU")
-getSusceptible = __columnGetter("susceptible")
-getLatent = __columnGetter("latent")
-getAsymptomatic = __columnGetter("asymptomatic")
-getPresymtomatic = __columnGetter("presymtomatic")
-getRecovered = __columnGetter("recovered")
-getDead = __columnGetter("dead")
+def gatherData(batchDir: str, experimentName: str) -> list:
+	# all the csv file that fit the format we wanted
+	dataFiles = [f for f in os.listdir(batchDir) if
+			path.isfile(path.join(batchDir, f))
+			and ("batchDetailed-" + experimentName in f)
+			and not ("building.csv" in f)]
+	# dictionary holds the total population data of each replication
+	# key = replication
+	replicationData = {}
+	for dataFile in dataFiles:
+		replicationIndex = dataFile.rsplit('_', 1)[0].rsplit("-", 1)[1]
+		data = pd.read_csv(path.join(batchDir, dataFile), dtype="int").reset_index(drop = True)
+		if replicationIndex not in replicationData:
+			replicationData[replicationIndex] = data
+		else:
+			replicationData[replicationIndex] += data
+	return list(replicationData.values())
